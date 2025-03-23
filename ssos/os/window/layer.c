2ef2e77a69697c46e8b2ff49beeb75adb3723b51
So Koide <sokoide@gmail.cojm>
2025-03-23 12:23:38 +0900

    refactor: faster drawing

--- ssos/os/window/layer.c
+++ ssos/os/window/layer.c
@@ -70,61 +70,64 @@ void ss_layer_set_z(Layer* layer, uint16_t z) {
 
 void ss_layer_draw() { ss_layer_draw_rect(0, 0, WIDTH, HEIGHT); }
 
+// Draw the rectangle area (x0, y0) - (x1, y1)
+// in vram coordinates
 void ss_layer_draw_rect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1) {
     for (int i = 0; i < ss_layer_mgr->topLayerIdx; i++) {
         Layer* layer = ss_layer_mgr->zLayers[i];
+        if (0 == layer->attr & LAYER_ATTR_VISIBLE)
+            continue;
+        // (dx0, dy0) - (dx1, dy1) is the intersection of the layer and the
+        // rectangle (x0, y0) - (x1, y1).
+        // layer->x+dx0, layer->y+dy0 - layer->x+dx1, layer->y+dy1 will be
+        // redrawn.
+        uint16_t dx0 = x0 - layer->x;
+        uint16_t dy0 = y0 - layer->y;
+        uint16_t dx1 = x1 - layer->x;
+        uint16_t dy1 = y1 - layer->y;
+        if (dx0 < 0)
+            dx0 = 0;
+        if (dy0 < 0)
+            dy0 = 0;
+        if (dx1 > layer->w)
+            dx1 = layer->w;
+        if (dy1 > layer->h)
+            dy1 = layer->h;
 
-        uint16_t bx0 = x0 - layer->x;
-        uint16_t by0 = y0 - layer->y;
-        uint16_t bx1 = x1 - layer->x;
-        uint16_t by1 = y1 - layer->y;
-        if (bx0 < 0)
-            bx0 = 0;
-        if (by0 < 0)
-            by0 = 0;
-        if (bx1 > layer->w)
-            bx1 = layer->w;
-        if (by1 > layer->h)
-            by1 = layer->h;
-        for (uint16_t by = by0; by < by1; by++) {
-            uint16_t vy = layer->y + by;
+        // (vx, vy) is the target vram coordinates to draw.
+        for (uint16_t dy = dy0; dy < dy1; dy++) {
+            uint16_t vy = layer->y + dy;
             if (vy < 0 || vy >= HEIGHT)
                 continue;
-            for (uint16_t bx = bx0; bx < bx1; bx++) {
-                uint16_t vx = layer->x + bx;
-                if (vx < 0 || vx >= WIDTH)
-                    continue;
+
+            // draw per 16byte block for faster drawing
+            uint16_t blocks = (dx1 - dx0) / 4;
+            uint16_t rest = (dx1 - dx0) % 4;
+            for (uint16_t b = 0; b < blocks; b++) {
+                uint16_t vx = layer->x + dx0 + b * 4;
+                uint16_t* src = &layer->vram[dy * layer->w + dx0 + b * 4];
+                volatile uint16_t* dst =
+                    &vram_start[vy * VRAMWIDTH + vx];
+                dst[0] = src[0];
+                dst[1] = src[1];
+                dst[2] = src[2];
+                dst[3] = src[3];
+            }
+            // draw the rest
+            for (uint16_t r = 0; r < rest; r++) {
+                uint16_t vx = layer->x + blocks * 4 + dx0 + r;
                 vram_start[vy * VRAMWIDTH + vx] =
-                    layer->vram[by * layer->w + bx];
+                    layer->vram[dy * layer->w + blocks * 4 + dx0 + r];
             }
         }
-        // if(layer->attr & LAYER_ATTR_VISIBLE){
-        //     for(int y=0;y<layer->h;y++){
-        //         int blocks = layer->w / 4;
-        //         int rest = layer->w % 4;
-        //         // draw per 16byte block for faster drawing
-        //         for(int b=0;b<blocks;b++){
-        //             uint16_t* src = &layer->vram[y * layer->w + b * 4];
-        //             uint16_t* dst = &vram_start[(layer->y + y) * VRAMWIDTH +
-        //             layer->x + b * 4]; dst[0] = src[0]; dst[1] = src[1];
-        //             dst[2] = src[2];
-        //             dst[3] = src[3];
-        //         }
-        //         // draw the rest
-        //         for(int r=0;r<rest;r++){
-        //             vram_start[(layer->y + y) * VRAMWIDTH + layer->x + blocks
-        //             * 4 + r] = layer->vram[y * layer->w + blocks * 4 + r];
-        //         }
-        //     }
-        // }
     }
 }
 
 void ss_layer_move(Layer* layer, uint16_t x, uint16_t y) {
-    uint16_t x0 = layer->x;
-    uint16_t y0 = layer->y;
+    uint16_t prevx = layer->x;
+    uint16_t prevy = layer->y;
     layer->x = x;
     layer->y = y;
-    ss_layer_draw_rect(x0, y0, x0 + layer->w, y0 + layer->h);
+    ss_layer_draw_rect(prevx, prevy, prevx + layer->w, prevy + layer->h);
     ss_layer_draw_rect(x, y, x + layer->w, y + layer->h);
 }
